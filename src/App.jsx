57330803bfa3c5984e8520c3539bdf8d82cfbd88
123456789afruz <afruzoxunov@gmail.com>
2024-10-22 18:59:15 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+import React from 'react'
+import Header from './components/Header/Header'
+import Hero from './components/Hero/Hero'
+import Footer from './components/Footer/Footer'
+
+const App = () => {
+  return (
+    <>
+      <Header/>
+      <Hero/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
